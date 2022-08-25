d9d315c26ec84773c85c57da20fae32e487519e2
Itamar Shechter <84578656+itamar-sh@users.noreply.github.com>
2022-08-25 11:57:17 +0300

    gd - first step

---
+++ ML/Gradient Descent/Generic Gradient Descent with constant learning rate.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from IMLearn import BaseModule
+
+
+class GradientDescent_constant_eta:
+    def __init__(self, learning_rate: float,
+                 tol: float = 1e-5,
+                 max_iter: int = 1000):
+        self.learning_rate_ = learning_rate
+        self.tol_ = tol
+        self.max_iter_ = max_iter
+
+    def fit(self, f: BaseModule, X: np.ndarray, y: np.ndarray):
+        output = []
+        for t in range(self.max_iter_):
+            gradient = f.compute_jacobian(X=X, y=y)   # dont necessarily use X, y. It can be constant. so return self.weight
+            step_val = self.learning_rate_ * gradient
+
+            f.weights = f.weights - step_val
+
+            output.append(f.weights)  # Average output
+
+            delta_val = np.linalg.norm(step_val)
+
+            if delta_val < self.tol_:
+                break
+
+        return np.mean(output, axis=0)  # average case
+
